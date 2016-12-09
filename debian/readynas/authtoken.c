784b1b95ff63ddd5661139e72b323189f33d26b0
NAStools <dev@nas.tools>
2016-12-09 10:20:28 -0800

    Clean up authtoken CGI

--- debian/readynas/authtoken.c
+++ debian/readynas/authtoken.c
@@ -3,14 +3,18 @@
 
 int main() {
 	FILE *f;
-	char c;
+	char c[32];
 
-	f=fopen("/apps/nastools-zerotier-one/var/authtoken.secret","rt");
+	f=fopen("/apps/nastools-zerotier-one/var/authtoken.secret","r");
+	if(!f)
+		return 1;
 
 	printf("Content-Type: text/plain\n\n");
 
-	while ((c=fgetc(f))!=EOF)
-		printf("%c", c);
+	size_t ret = fread(c, 1, sizeof(c), f);
+	if(!ret)
+		return 1;
+	fwrite(c, ret, 1, stdout);
 
 	fclose(f);
 	return 0;
